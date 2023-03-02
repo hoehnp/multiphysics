946dcbe5b73c4d6eda349f0653c9debffa6856cd
Patrick Höhn <pho17@tu-clausthal.de>
2023-03-02 20:35:03 +0100

    added script and changes for building on windows

---
+++ energy2d/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=$PATH:"/C/Program Files (x86)/Inno Setup 6/"
+# one has to patch template.iss in ant-javafx.jar to change min version
+
+/C/Users/pho17/Downloads/apache-ant-1.10.13/bin/ant default
